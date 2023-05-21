553fab7e2c0be957b82561484bda3299aebb23a6
spark1128 <stevepark1128@gmail.com>
2023-05-21 16:25:32 -0700

    init order component and service added

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrderComponent } from './order/order.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'order',
+    component: OrderComponent
+  },
+  { path: '**', component: OrderComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
